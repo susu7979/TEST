fb0bd7cb5a81e398bd3c233abaf28e23cc141204
susu7979 <sophie790709@hotmail.com>
2017-07-13 13:36:27 +0800

    String

---
+++ src/TEST1/string.java
@@ -0,0 +1,18 @@
+package TEST1;
+
+public class string {
+
+	public static void main(String[] args) {
+		String string="Susu is a pretty girl.";
+		System.out.println(string.charAt(3));
+		System.out.println(string.contains("s"));
+		System.out.println(string.replace('s', 'h'));
+		System.out.println(string.toCharArray());
+		System.out.println(string.toLowerCase());
+		System.out.println(string.toUpperCase());
+		System.out.println(string.length());
+		System.out.println(string.startsWith("a"));
+		System.out.println(string.split(" ")[0]);
+	}
+
+}
